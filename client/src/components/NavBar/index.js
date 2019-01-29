35aabd8b577615d977c6c1f50a14c91070002089
Rosyming <41979276+rosyming@users.noreply.github.com>
2019-01-29 17:16:35 -0500

    Minor changes to Navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -15,13 +15,19 @@ function NavBar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Login <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#">Login/Account <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Donate <span className="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
