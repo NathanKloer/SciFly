6f07e92c234c3003ba08ebd93a1939aa8eaee77b
Rosyming <41979276+rosyming@users.noreply.github.com>
2019-01-29 19:30:20 -0500

    Minor changes

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,42 +3,6 @@ import { Modal, Navbar, Nav, NavItem, OverlayTrigger, Tooltip } from "react-boot
 import { Input, FormBtn, Dropdown, DropdownList } from "../LogIn";
 import API from "../../utils/API";
 // import { Link } from "react-router-dom";
-<<<<<<< HEAD
-// import "./style.css";
-
-function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="#">
-        Welcome to Parts to Purpose
-      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Login/Account <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Donate <span className="sr-only">(current)</span></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-}
-=======
 import "./style.css";
 
 class NavBar extends Component {
@@ -328,6 +292,5 @@ class NavBar extends Component {
       )
     }
     }
->>>>>>> master
 
 export default NavBar;
