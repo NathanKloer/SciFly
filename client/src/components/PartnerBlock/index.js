98bf9f38e43713bc08038a218d5083c0f63b4785
Rosyming <41979276+rosyming@users.noreply.github.com>
2019-02-12 23:54:17 -0500

    Found duplicated paragraph in Donor page

--- client/src/components/PartnerBlock/index.js
+++ client/src/components/PartnerBlock/index.js
@@ -59,12 +59,11 @@ function PartnerBlock() {
               <p>
                 <br /><br />
                 <MDBIcon icon="quote-left" />  Many small nonprofits receive item donations from donors and distribute those items to people in need.  A lot of these nonprofits operate on a shoestring budget and use basic tools such as Microsoft Excel, Word, and Outlook to manage their inventory, interact with donors, and distribute donations to donees. Their inventory, donees, and donor management are at times kept on multiple documents in different locations and managed by various volunteers which makes it difficult to update, maintain, access, and scale.
-          <br /><br />
-                Our goal is to provide an IT solution that would make the process easier for small nonprofits to manage their donations so that they can spend more time working on their cause -- making a difference.  Our app is called Parts-to-Purpose.  It is a simple inventory and people management platform for nonprofits that will allow them to:
+
           <br /><br />
           Our goal is to provide an IT solution that would make the process easier for small nonprofits to manage their donations so that they can spend more time working on their cause -- making a difference.  Our app is called Parts-to-Purpose.  It is a simple inventory and people management platform for nonprofits that will allow them to:
           </p>
-          <br /><br />
+
           <ul>
             <li>Update, manage, and track their inventory of donations</li>
             <li>Collect donor and donee information</li>
@@ -72,7 +71,7 @@ function PartnerBlock() {
             <li>See changes in inventory/stock in near real-time</li>
             <li>Collect and visualize metrics to identify needs and trends so that they can use those metrics to focus donation efforts or community marketing</li>
           </ul>
-          <br />
+
           <p>
           The value of our application is that it will be an open-source, inexpensive, and simple inventory solution that nonprofits can use to immediately connect the “purpose” in need to the “part” that’s needed. <MDBIcon icon="quote-right" />
           </p>
